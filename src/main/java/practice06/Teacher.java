524b00c3589461aba6ae48f7eda372a411054e9b
CANCEAL <alfred.cancejo@oocl.com>
2020-10-14 22:18:45 +0800

    [OOP] [Alfred Cancejo] Feature: Practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String firstString = "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. ";
+        String secondString = "I teach Class " + getKlass() + ".";
+        String secondStringNull = "I teach No Class.";
+
+        return klass != 0 ? firstString + secondString : firstString + secondStringNull;
+    }
 }
