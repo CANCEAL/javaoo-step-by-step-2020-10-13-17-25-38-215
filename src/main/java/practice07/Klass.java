a4e022a45703d12240297d083a3b48a89d2df286
CANCEAL <alfred.cancejo@oocl.com>
2020-10-14 22:20:10 +0800

    [OOP] [Alfred Cancejo] Feature: Practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,21 @@
 package practice07;
 
 public class Klass {
+    int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Klass() {
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + getNumber();
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,23 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    Klass klass = new Klass();
+
+    public Klass getKlass(){
+        return klass;
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getNumber(){
+        return klass.getNumber();
+    }
+
+    public String introduce() {
+        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Student. I am at Class "+ klass.getNumber() + ".";
+    }
+
 }
