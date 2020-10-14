2033b7c82d29489de2043e533e026d7d81d662f2
CANCEAL <alfred.cancejo@oocl.com>
2020-10-14 16:22:26 +0800

    [OOP] [Alfred Cancejo] Feature: Practice03

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,20 @@
 package practice03;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+    int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass(){
+        return klass;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class " + getKlass() + ".";
+    }
 }
