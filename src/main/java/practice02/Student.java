56d859c22bd8180a72f96341d281f020a3b253ab
CANCEAL <alfred.cancejo@oocl.com>
2020-10-14 22:15:24 +0800

    [OOP] [Alfred Cancejo] Feature: Practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -13,7 +13,7 @@ public class Student extends Person{
     }
 
     public String introduce() {
-        return "I am a Student. I am at Class " + getKlass() + ".";
+        return "I am a Student. I am at Class "  + getKlass() + ".";
     }
 
 }
