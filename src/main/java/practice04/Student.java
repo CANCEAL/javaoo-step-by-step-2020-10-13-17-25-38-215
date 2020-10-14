1d828544af48a0440a87441ea7bdb21507552861
CANCEAL <alfred.cancejo@oocl.com>
2020-10-14 16:23:23 +0800

    [OOP] [Alfred Cancejo] Feature: Practice04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,21 @@
 package practice04;
 
-public class Student {
+import practice03.Person;
+
+public class Student extends Person {
+    int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass(){
+        return klass;
+    }
+
+    public String introduce() {
+        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Student. I am at Class "+ getKlass() + ".";
+        //My name is Tom. I am 21 years old.I am a Student. I am at Class 2.
+    }
 }
