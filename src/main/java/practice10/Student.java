8428828a90c1b2682163f2a5e6f19b2846d6c6d7
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 21:34:26 +0800

    [OOP] [Alfred Cancejo] Feature: Practice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,24 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    Klass klass = new Klass();
+
+    public Klass getKlass(){
+        return klass;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public int getNumber(){
+        return klass.getNumber();
+    }
+
+    public String introduce() {
+        return klass.getLeader() != null ? super.introduce() + " I am a Student. I am Leader of Class " + getNumber() + "."
+                : super.introduce() + " I am a Student. I am at Class " +  getNumber() + ".";
+    }
+
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,62 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    final String teacherIntroduction = " I am a Teacher. I ";
+
+    Klass klass = new Klass();
+    LinkedList<Klass> linkedList = new LinkedList<Klass>();
+    private int classNumber;
+
+    public Klass getKlass(){
+        return klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> linkedList) {
+        super(id, name, age);
+        this.linkedList = linkedList;
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public int getNumber() {
+        return klass.getNumber();
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return linkedList;
+    }
+
+    public boolean isTeaching(Student student) {
+        if (getClasses().contains(student.getKlass())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String introduce(){
+        if(getClasses().size() != 0){
+            String classNumbers = "";
+            for(Klass klass: getClasses()){
+                classNumbers += klass.getNumber() + ", ";
+            }
+            return super.introduce() + teacherIntroduction + "teach Class " + classNumbers.substring(0,classNumbers.length()-2) + ".";
+        }
+        return super.introduce() + teacherIntroduction + "teach No Class.";
+    }
+
+    public String introduceWith(Student studentName) {
+        classNumber = getClasses().indexOf(studentName.getKlass());
+        return classNumber == 0 ? super.introduce() + teacherIntroduction + "teach " + studentName.getName() + "." :
+                super.introduce() + teacherIntroduction + "don't teach " +  studentName.getName() + ".";
+    }
 }
