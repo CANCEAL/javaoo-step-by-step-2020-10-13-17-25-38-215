b9ab53042def34f6d70240ac7696415a62895edc
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 21:34:02 +0800

    [OOP] [Alfred Cancejo] Feature: Practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,37 @@
 package practice09;
 
 public class Klass {
+    int number;
+    public Student studentLeader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Klass() {
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    public Student getLeader() {
+        return studentLeader;
+    }
+
+    public void assignLeader(Student student) {
+        if(this.number == student.getNumber()) {
+            this.studentLeader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student jerry) {
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,33 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    final String teacherIntroduction = " I am a Teacher. I ";
+    Klass klass = new Klass();
+
+    public Klass getKlass(){
+        return klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public int getNumber() {
+        return klass.getNumber();
+    }
+
+    public String introduce() {
+        return getNumber() != 0 ? super.introduce() + teacherIntroduction + "teach Class " + getNumber() + "." :
+                super.introduce() + teacherIntroduction + "teach No Class.";
+    }
+
+    public String introduceWith(Student studentName) {
+        return klass.getNumber() == studentName.getNumber() ? super.introduce() + teacherIntroduction + "teach " + studentName.getName() + "."
+                : super.introduce() + teacherIntroduction + "don't teach " + studentName.getName() + ".";
+    }
 }
